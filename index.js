305842856f3e97a168804cef46640bab893c754e
Pablo <pablomurbaez@yahoo.com>
2023-11-30 00:13:48 -0500

    completed lab

---
+++ index.js
@@ -0,0 +1,10 @@
+function introduction(name) {   
+    return `Hi, my name is ${name}.`;
+}
+console.log(introduction("name"));
+function introductionWithLanguage(name, language) {
+    return "Hi, my name is " + name + " and I am learning to program in " + language + ".";
+}
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return "Hi, my name is " + name + " and I am learning to program in " + language + "."; 
+}
